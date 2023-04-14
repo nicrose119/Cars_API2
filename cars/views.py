e5ca4f715f42fab35d18c5b1e6fc85b897380586
Nicole Overholt <nicrose119@gmail.com>
2023-04-14 03:53:39 -0400

    Updated Views.py file imported CarSerializer and Car model + set up postman

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
+
 
 @api_view(['GET'])
 def cars_list(request):
+    cars = Car.objects.all()
+    
+    serializer = CarSerializer(cars, many=True)
 
-    return Response('Ok')    
+    return Response(serializer.data)
+    
