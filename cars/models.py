17b541bc703b9b12e90d6c2070c96545d63cc58d
Nicole Overholt <nicrose119@gmail.com>
2023-04-18 03:55:13 -0400

    Added a date added parameter to Car Model + updated serializers and cars file

--- cars/models.py
+++ cars/models.py
@@ -6,5 +6,6 @@ class Car(models.Model):
     make = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     year = models.IntegerField()
-    price = models.DecimalField(max_digits=8, decimal_places = 2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_added = models.DateField(auto_now_add=True)
       
