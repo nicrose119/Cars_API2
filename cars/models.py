7307fa625c735a2db266efdbc097d6fc00cc8239
Nicole Overholt <nicrose119@gmail.com>
2023-04-18 04:40:52 -0400

    Created Dealership App, serializer & urls files, and dealership model. Updated Cars app views file, models file and serializer file

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from dealerships.models import Dealership
 # Create your models here.
 
 class Car(models.Model):
@@ -8,4 +8,4 @@ class Car(models.Model):
     year = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     date_added = models.DateField(auto_now_add=True)
-      
+    dealership = models.ForeignKey(Dealership, on_delete=models.CASCADE)
